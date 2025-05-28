bc9c542a20c7a4069627679d53dbf4ea7bfa2674
Vaidul1S <vaidas.ad25@gmail.com>
2025-05-28 09:48:20 +0300

    05.28

---
+++ index17.php
@@ -0,0 +1,19 @@
+<?php
+    //include() function
+    include("components17/header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include</title>
+</head>
+<body>
+    Welcome to Homepage!<br>
+    Stuff about your homepage goes here.
+</body>
+</html>
+<?php
+        include("components17/footer.html");
+?>
