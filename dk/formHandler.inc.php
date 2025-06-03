970071064448b223daf289d10f75eb8febfb353a
Vaidul1S <vaidas.ad25@gmail.com>
2025-06-03 11:10:26 +0300

    06.03

--- dk/formHandler.inc.php
+++ dk/formHandler.inc.php
@@ -2,10 +2,27 @@
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = $_POST["username"];
-    $password = $_POST["password"];
+    $pwd = $_POST["password"];
 
     try {
         require_once "db.inc.php";
+        $query = "INSERT INTO planets (user, password) 
+                VALUES (:user, :pwd);";
+
+        $stmt = $pdo->prepare($query);
+
+        $stmt->bindParam(":user", $username);
+        $stmt->bindParam(":pwd", $pwd);
+
+        // $stmt->execute([$username, $pwd]);               // naudojam kai VALUES(?, ?)
+        $stmt->execute();
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: index.php");
+        die();
+        
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
     }
