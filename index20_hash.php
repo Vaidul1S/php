3584bc9ad389c3f6370a61c8592cfd19e35b73f8
Vaidul1S <vaidas.ad25@gmail.com>
2025-06-04 12:52:46 +0300

    06.04

--- index20_hash.php
+++ index20_hash.php
@@ -13,6 +13,7 @@
         echo"<p>Incorect password </p>";
     }
 
+    //DK simple hashing
     $sensitiveData = "Meovv";
 
     $salt = bin2hex(random_bytes(16));
@@ -26,15 +27,17 @@
     echo "<p>Hash2: {$hash2}</p>";
 
     $storedSalt = $salt;
-    $storesHash2 = $hash2;
+    $storedHash2 = $hash2;
     $pepper = "ASecretPepperSring";
 
     $dataToHash = $sensitiveData . $salt . $pepper;
 
     $verificationHash = hash("sha256", $dataToHash);
 
-    if($storesHash2 ===$verificationHash){
+    if($storedHash2 ===$verificationHash){
         echo "<p>The data are the same!</p>";
+        echo "<p>{$storedHash2}</p>";
+        echo "<p>{$verificationHash}</p>";
     }else {
         echo "<p>The data aren't the same!</p>";
     }
