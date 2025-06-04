4b06f10a7759d44a219079776815ea96fad518f2
Vaidul1S <vaidas.ad25@gmail.com>
2025-06-04 13:49:36 +0300

    06.04

---
+++ login/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Login Site</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="formHandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username"><br>
+        <input type="password" name="password" placeholder="password"><br>        
+        <button>Sign Up</button>
+    </form>
+</body>
+</html>
