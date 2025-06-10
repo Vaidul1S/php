138821945070902679a6ed2c50d7c01f95c08554
Vaidul1S <vaidas.ad25@gmail.com>
2025-06-10 11:22:13 +0300

    06.10

--- login/includes/config_session.inc.php
+++ login/includes/config_session.inc.php
@@ -12,16 +12,38 @@ session_set_cookie_params([
 
 session_start();
 
- function regenerate(){
+function regenerate(){
+    session_regenerate_id(true);
+
+    $userID = $_SESSION["user_id"];
+    $newSessionId = session_create_id();
+    $sessionId = $newSessionId . "_" . $userID;
+    session_id($sessionId);
+
+    $_SESSION['last_regeneration'] = time();
+}
+
+function regenerate_session_id_loggedin(){
     session_regenerate_id(true);
     $_SESSION['last_regeneration'] = time();
- }
+}
 
-if(!isset($_SESSION["last_regeneration"])){
-    regenerate();
+if(isset($_SESSION["user_id"])){
+    if(!isset($_SESSION["last_regeneration"])){
+        regenerate_session_id_loggedin();
+    } else {
+        $interval = 60 * 30;
+        if(time() - $_SESSION["last_regeneration"] >= $interval){
+            regenerate_session_id_loggedin();
+        }
+    }  
 } else {
-    $interval = 60 * 30;
-    if(time() - $_SESSION["last_regeneration"] >= $interval){
+    if(!isset($_SESSION["last_regeneration"])){
         regenerate();
-    }
-} 
+    } else {
+        $interval = 60 * 30;
+        if(time() - $_SESSION["last_regeneration"] >= $interval){
+            regenerate();
+        }
+    }  
+}
--- login/includes/login.inc.php
+++ login/includes/login.inc.php
@@ -20,18 +20,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         if(user_doesnt_exists($result)){
             $errors["login_incorrect"]= "User not found!";
         }
+        if(!user_doesnt_exists($result) && is_pwd_wrong($password, $result["password"])){
+            $errors["login_incorrect"]= "Check username or password!";
+        }
 
         require_once 'config_session.inc.php';
 
         if($errors){
             $_SESSION["errors_signup"] = $errors;
-            $signup_data = [
-                'username' => $username    
-            ];
-            $_SESSION["signup_data"] = $signup_data;
+            
             header("Location: ../index.php");
             die();
         }
+
+        $newSessionId = session_create_id();
+        $sessionId = $newSessionId . "_" . $result["id"];
+        session_id($sessionId);
+
+        $_SESSION["user_id"] = $result["id"];
+        $_SESSION["user_username"] = htmlspecialchars($result["username"]);
+
+        $_SESSION['last_regeneration'] = time();
+
+        header("Location: ../index.php?login=success");
+
+        $pdo = null;
+        $stmt = null;
+        die();
+        
     } catch (PDOException $e) {
         die("Querry failed: " . $e->getMessage());
     }
