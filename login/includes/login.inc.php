fd29202b5f7705703335dd6ad762f32605953e8b
Vaidul1S <vaidas.ad25@gmail.com>
2025-06-10 10:39:12 +0300

    06.10

--- login/includes/login.inc.php
+++ login/includes/login.inc.php
@@ -4,6 +4,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = $_POST["username"];
     $password = $_POST["password"];
 
+    try {
+        
+    } catch (PDOException $e) {
+        die("Querry failed: " . $e->getMessage());
+    }
 } else {
     header("Location: ../index.php");
     die();
