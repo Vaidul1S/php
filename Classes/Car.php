ddc09c379433cad512b10359b73478177f9151cb
Vaidul1S <vaidas.ad25@gmail.com>
2025-06-11 11:31:54 +0300

    06.11

--- Classes/Car.php
+++ Classes/Car.php
@@ -2,11 +2,13 @@
 
 class Car {
     //Properties / Fields
-    private $brand;           // private info tik sitai klasei ir nepereina subklasem
+    private $brand;                // private info tik sitai klasei ir nepereina subklasem
     private $color;                // protected info pereina sub klasem
-    private $year;                      // public info visiems, default turetu but private visiems
-
+    private $year;                 // public info visiems, default turetu but private visiems
+    private $vehicleType = "car";
     //Constrtuctor
-    
 
+    public function __construct($brand, $color, $year) {
+        
+    }
 }
