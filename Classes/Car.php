451f1bf701c1a9a78b1c0250eab9885a82d75704
Vaidul1S <vaidas.ad25@gmail.com>
2025-06-11 11:28:47 +0300

    06.11

--- Classes/Car.php
+++ Classes/Car.php
@@ -1,2 +1,12 @@
 <?php
 
+class Car {
+    //Properties / Fields
+    private $brand;           // private info tik sitai klasei ir nepereina subklasem
+    private $color;                // protected info pereina sub klasem
+    private $year;                      // public info visiems, default turetu but private visiems
+
+    //Constrtuctor
+    
+
+}
